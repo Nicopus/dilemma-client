84b3a147675013436feca733c8885ad65ec53e4b
nicolaihansen <nicolaihansen@live.dk>
2016-04-19 13:47:58 +0200

    oprettet project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dilemmaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dilemmaApp
+ */
+angular.module('dilemmaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dilemmaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dilemmaApp
+ */
+angular.module('dilemmaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
