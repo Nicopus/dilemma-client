e5c6848c23f6d341f732d8a5d91d686d1f80b373
nicolaihansen <nicolaihansen@live.dk>
2016-04-21 16:48:22 +0200

    semi færdig med opret dilemma

--- app/scripts/controllers/opret-dilemma.js
+++ app/scripts/controllers/opret-dilemma.js
@@ -7,7 +7,20 @@
  * # OpretDilemmaCtrl
  * Controller of the dilemmaApp
  */
-angular.module('dilemmaApp')
-  .controller('OpretDilemmaCtrl', function ($scope) {
+ angular.module('dilemmaApp')
+ .controller('OpretDilemmaCtrl', function ($scope, $http) {
+ 	$scope.opretDilemma = function(dilemma){
+ 		$http.post("http://localhost:3001/d/opret", {
+ 				"name" : dilemma.quest,
+ 				"desc" : dilemma.des,
+ 				"alvor" : 2,
+ 				"p_answers" : [{"text" : dilemma.opt1}, {"text" :dilemma.opt2}]
+ 		}).success(function(data, status, headers, config){
 
-  });
+ 		}).error(function(data, status, headers, config){
+
+ 		})
+ 	};
+
+ });
+
