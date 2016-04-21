2eda6dde25c47b3fb2f988622cd8f6a6c62f83a0
atila29 <mathias.petersen95@gmail.com>
2016-04-21 18:37:28 +0200

    first

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,13 +10,7 @@
 angular.module('dilemmaApp')
   .controller('LoginCtrl', function ($scope, $http) {
     $scope.login = function(user){
-      $http({
-        method: "POST",
-        url: "http://localhost:3001/login",
-        data: {
-          user : user.username, password : user.password
-        }
-      }).
+      $http.post("http://localhost:3001/login", {"user" : user.username, "password" : user.password}).
       success(function(data, status, headers, config){
 
       }).error(function(data, status, headers, config){
