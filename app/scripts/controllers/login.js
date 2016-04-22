0e73201b102d4e1a89751b0942e3418242fba981
atila29 <mathias.petersen95@gmail.com>
2016-04-22 03:14:10 +0200

    update - login virker, dog mangler
    stoarage

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -12,9 +12,15 @@ angular.module('dilemmaApp')
     $scope.login = function(user){
       $http.post("http://localhost:3001/login", {"user" : user.username, "password" : user.password}).
       success(function(data, status, headers, config){
-
+        var token = data.token;
+        if(token){
+          $scope.user.username = "correct";
+        }
+        else {
+          $scope.user.username = "wrong";
+        }
       }).error(function(data, status, headers, config){
-
+        $scope.user.username = "error";
       })
     };
   });
