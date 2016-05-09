ec47071a88bb4711052d7b6a91d483dc45135af8
nicolaihansen <nicolaihansen@live.dk>
2016-05-09 14:25:41 +0200

    vælg alvor virker

--- app/scripts/controllers/opret-dilemma.js
+++ app/scripts/controllers/opret-dilemma.js
@@ -9,58 +9,60 @@
  */
  angular.module('dilemmaApp')
  .controller('OpretDilemmaCtrl', function ($window, $scope, $http, $timeout, localStorageService, Upload) {
- //
+ 
  $scope.test = localStorageService.get('tok');
+
+ $scope.activeGravity = $scope.bot1;
 
  $scope.dilemma = {
    quest : 'spg',
    desc : 'd',
-   alvor : 2,
+   alvor : 0,
    p_answers : [
    {text : 'test'},
    {text : 'test2'}
    ]
  };
 
- $scope.create = function(dilemma){
-   var files = [];
-   var options = [];
+$scope.create = function(dilemma){
+ var files = [];
+ var options = [];
 
-   if($scope.dilemmaForm.opt1.$valid) {
-    options.push({"text" : dilemma.opt1});
-    if($scope.picfile1) {
-      files.push($scope.picfile1);
+ if($scope.dilemmaForm.opt1.$valid) {
+  options.push({"text" : dilemma.opt1});
+  if($scope.picfile1) {
+    files.push($scope.picfile1);
 
-    }
   }
-  if($scope.dilemmaForm.opt2.$valid) {
-    options.push({"text" : dilemma.opt2});
-    if($scope.picfile2) {
-      files.push($scope.picfile2);
+}
+if($scope.dilemmaForm.opt2.$valid) {
+  options.push({"text" : dilemma.opt2});
+  if($scope.picfile2) {
+    files.push($scope.picfile2);
 
-    }
   }
-  if($scope.dilemmaForm.opt3.$valid) {
-    options.push({"text" : dilemma.opt3});
-    if($scope.picfile3) {
-      files.push($scope.picfile3);
+}
+if($scope.dilemmaForm.opt3.$valid) {
+  options.push({"text" : dilemma.opt3});
+  if($scope.picfile3) {
+    files.push($scope.picfile3);
 
-    }
   }
-  if($scope.dilemmaForm.opt4.$valid) {
-    options.push({"text" : dilemma.opt4});
-    if($scope.picfile4) {
-      files.push($scope.picfile4);
+}
+if($scope.dilemmaForm.opt4.$valid) {
+  options.push({"text" : dilemma.opt4});
+  if($scope.picfile4) {
+    files.push($scope.picfile4);
 
-    }
   }
-  if($scope.dilemmaForm.opt5.$valid) {
-    options.push({"text" : dilemma.opt5});
-    if($scope.picfile5) {
-      files.push($scope.picfile5);
+}
+if($scope.dilemmaForm.opt5.$valid) {
+  options.push({"text" : dilemma.opt5});
+  if($scope.picfile5) {
+    files.push($scope.picfile5);
 
-    }
   }
+}
 
   //felter skal udfyldes. -> skal også gøres server-side
   if(options.length < 2){
@@ -86,7 +88,7 @@
    $scope.test = resp;
    if(resp.data.success){
     $window.location.href = '#/';
-   }
+  }
 }, function (resp) {
   console.log('Error status: ' + resp.status);
 }, function (evt) {
