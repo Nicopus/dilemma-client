446092ec706a31aeb9e348a8b0ed52e2b863bd5e
atila29 <mathias.petersen95@gmail.com>
2016-05-13 16:16:30 +0200

    kan nu stemme på dilemma'er
    der kan nu stemmes på dilemma
    
    lav evt brugerprofil, og ret fejl -> ftp next()

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # dilemma
 
+This project is mad by:
+s14487, s133974 & s144884
+project for Distributed Systems [02342]
+##tools
+-angular
+-grunt
+-yeoman
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
 version 0.15.1.
 
--- app/scripts/controllers/svar-dilemma.js
+++ app/scripts/controllers/svar-dilemma.js
@@ -43,7 +43,24 @@
      })
      .success(function(data, status, headers, config){
        //$scope.test = JSON.stringify(data);
-       getStat();
+       getAnswered();
+     })
+     .error(function(data, status, headers, config){
+
+     });
+   };
+
+   var getAnswered = function () {
+     $http({
+       url : irl_answered,
+       method : "GET",
+       params : {
+         'token' : localStorageService.get('tok')
+       }
+     })
+     .success(function(data, status, headers, config){
+       //$scope.test = JSON.stringify(data);
+       callback_answered(data);
      })
      .error(function(data, status, headers, config){
 
@@ -78,78 +95,6 @@
 
    });
 
-   $http({
-     url : irl_answered,
-     method : "GET",
-     params : {
-       'token' : localStorageService.get('tok')
-     }
-   })
-   .success(function(data, status, headers, config){
-     //$scope.test = JSON.stringify(data);
-     callback_answered(data);
-   })
-   .error(function(data, status, headers, config){
-
-   });
-
-
- 	var voted = false;
-
- 	const after_vote_html =
- 	'<div class="progress">' +
- 	'<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 10%;">' +
- 	'<span>Ja (10%)</span>' +
- 	'</div>' +
- 	'</div>' +
- 	'<div class="progress">' +
- 	'<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">' +
- 	'<span>Nej (50%)</span>' +
- 	'</div>' +
- 	'</div>' +
- 	'<div class="progress">' +
- 	'<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 15%;">' +
- 	'<span>Måske (15%)</span>' +
- 	'</div>' +
- 	'</div>' +
- 	'<div class="progress">' +
- 	'<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 2%;">' +
- 	'<span>Senere (2%)</span>' +
- 	'</div>' +
- 	'</div>' +
- 	'<div class="progress">' +
- 	'<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 28%;">' +
- 	'<span>Aldrig (28%)</span>' +
- 	'</div>' +
- 	'</div>';
-
- 	const before_vote_html =
- 	'<div class="row grid5">' +
- 	'<div class="col-md-1 text-center">' +
- 	'<button type="button" ng-click="vote()" class="btn btn-lg btn-primary" min>Ja</button>' +
- 	'</div>' +
- 	'<div class="col-md-1 text-center">' +
- 	'<button type="button" ng-click="vote()" class="btn btn-lg btn-primary">Nej</button>' +
- 	'</div>' +
- 	'<div class="col-md-1 text-center">' +
- 	'<button type="button" ng-click="vote()" class="btn btn-lg btn-primary">Måske</button>' +
- 	'</div>' +
- 	'<div class="col-md-1 text-center">' +
- 	'<button type="button" ng-click="vote()" class="btn btn-lg btn-primary">Aldirg</button>' +
- 	'</div>' +
- 	'<div class="col-md-1 text-center">' +
- 	'<button type="button" ng-click="vote()" class="btn btn-lg btn-primary">Senere</button>' +
- 	'</div>' +
- 	'</div>';
-
- 	if (!voted) {
- 		$scope.context = before_vote_html;
- 	} else {
- 		$scope.context = after_vote_html;
- 	}
-
-
-
-
+   getAnswered();
 
  });
